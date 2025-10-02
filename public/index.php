be633f74bba187bff1dd59d8774d33c1af0e28f9
TanguyVaucher <1999tanguy@gmail.com>
2025-10-02 14:02:01 +0200

    Structure2

--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TeamUP - Site officiel</title>
+    <link rel="stylesheet" href="assets/global.css">
 </head>
+
 <body>
-    
+    <?php require __DIR__ . '/menus/header.php'; ?>
+
+    <main>
+        <h1>Bienvenue sur TeamUP!</h1>
+        <p>Choisissez un sport.</p>
+    </main>
+
+    <?php require __DIR__ . '/menus/footer.php'; ?>
 </body>
+
 </html>
