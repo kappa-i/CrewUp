25d887c0cfc20ac6f1691c1b9bc6978fe1ac2c56
TanguyVaucher <1999tanguy@gmail.com>
2025-10-13 16:19:39 +0200

    Header + Main (HTML, CSS)

---
+++ public/menus/header.php
@@ -0,0 +1,11 @@
+<nav class="nav" aria-label="Navigation principale">
+  <h1 class="logo"><a href="/">CrewUp</a></h1>
+
+  <ul class="main-menu">
+    <li><a href="/">Accueil</a></li>
+    <li><a href="/annonces">Annonces</a></li>
+    <li><a href="/terrains">Terrains</a></li>
+  </ul>
+
+  <a class="btn-inscription" href="/inscription">Inscription gratuite</a>
+</nav>
