2c1dd6fbe068081f82be87864cba6654708d93f5
TanguyVaucher <1999tanguy@gmail.com>
2025-10-14 10:16:38 +0200

    links patch

--- public/menus/header.php
+++ public/menus/header.php
@@ -3,8 +3,8 @@
 
   <ul class="main-menu">
     <li><a href="/">Accueil</a></li>
-    <li><a href="/annonces">Annonces</a></li>
-    <li><a href="/terrains">Terrains</a></li>
+    <li><a href="/annonces.php">Annonces</a></li>
+    <li><a href="/terrains.php">Terrains</a></li>
   </ul>
 
   <a class="btn-inscription" href="/account/dashboard.php">Inscription gratuite</a>
