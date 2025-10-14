b336d91d223a4981fdd445e88076f4cd5ed34d99
TanguyVaucher <1999tanguy@gmail.com>
2025-10-14 10:42:01 +0200

    t4

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="assets/css/global.css">
     <link rel="stylesheet" href="https://use.typekit.net/ooh3jgp.css">
     <script src="assets/js/global.js"></script>
-    <link rel="icon" href="https://crewup.ch/favicon.ico">
-    <link rel="shortcut icon" href="https://crewup.ch/favicon.ico">
+    <link rel="icon" href="https://crewup.ch/favicon.ico?v=6" sizes="any">
+    <link rel="shortcut icon" href="https://crewup.ch/favicon.ico?v=6">
 
 </head>
 
