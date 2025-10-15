15f5a9b1629830e595c53acdb273b2c69a49521a
kappa-i <gaby.kappai@gmail.com>
2025-10-15 18:05:42 +0200

    Update update.php

--- public/account/update.php
+++ public/account/update.php
@@ -246,35 +246,3 @@ if (isset($_GET["id"])) {
     <?php require __DIR__ . '/../menus/footer.php'; ?>
 </body>
 
-
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CrewUp - Édition</title>
-    <link rel="stylesheet" href="../assets/css/global.css">
-    <link rel="stylesheet" href="https://use.typekit.net/ooh3jgp.css">
-    <script src="../assets/js/global.js"></script>
-    <link rel="shortcut icon" href="../favicon.ico">
-    <link rel="icon" href="https://crewup.ch/favicon.ico?v=6" sizes="any">
-</head>
-
-<body>
-    <?php require __DIR__ . '/../menus/header.php'; ?>
-
-    <main>
-        <h1 class="hello">Éditer une annonce</h1>
-        <ul class="account-menu" style="margin-bottom: 400px;">
-            <li><a href="dashboard.php">• Retour</a></li>
-            <li><a href="create.php">• Créer</a></li>
-            <li><a href="delete.php">• Supprimer</a></li>
-        </ul>
-    </main>
-
-    <?php require __DIR__ . '/../menus/footer.php'; ?>
-</body>
-
-</html>
