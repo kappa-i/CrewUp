ed7b222572da8b2ec521e149db291e9823bf29fc
TanguyVaucher <1999tanguy@gmail.com>
2025-10-28 16:15:15 +0100

    push fix

--- bug_bcp/src/Classes/Users/UserManagerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Users;
-
-interface UsersManagerInterface {
-    public function getUsers(): array;
-    public function addUser(User $user): int;
-    public function removeUser(int $id): bool;
-}
