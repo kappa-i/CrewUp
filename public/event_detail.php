a11abc84e383bbce13d062c4050817b10e12d2cc
kappa-i <gaby.kappai@gmail.com>
2025-11-01 21:46:28 +0100

    .

--- public/event_detail.php
+++ public/event_detail.php
@@ -41,8 +41,8 @@ $sports = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CrewUp - <?php echo htmlspecialchars($event->getTitle()); ?></title>
-    <link rel="stylesheet" href="assets/css/global.css">
-    <link rel="stylesheet" href="assets/css/event-detail.css">
+    <link rel="stylesheet" href="/assets/css/global.css">
+    <link rel="stylesheet" href="/assets/css/event-detail.css">
     <link rel="stylesheet" href="https://use.typekit.net/ooh3jgp.css">
     <script src="assets/js/global.js"></script>
     <link rel="icon" href="https://crewup.ch/favicon.ico?v=6" sizes="any">
