43b787283bfea9914a4715beb87c1d2924b43e67
TanguyVaucher <1999tanguy@gmail.com>
2025-11-18 23:11:19 +0100

    t4

--- public/smtp_test.php
+++ public/smtp_test.php
@@ -1,48 +1,43 @@
 <?php
 
-// Charger PHPMailer depuis TA structure
-require __DIR__ . '/../src/PHPMailer/PHPMailer.php';
-require __DIR__ . '/../src/PHPMailer/SMTP.php';
-require __DIR__ . '/../src/PHPMailer/Exception.php';
+require __DIR__ . '/../src/PHPMailer/PHPMailer/PHPMailer.php';
+require __DIR__ . '/../src/PHPMailer/PHPMailer/SMTP.php';
+require __DIR__ . '/../src/PHPMailer/PHPMailer/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-echo "<pre>TEST SMTP…\n";
+echo "<pre>START SMTP TEST\n";
 
 $mail = new PHPMailer(true);
 
 try {
-    // Debug max
+
     $mail->SMTPDebug = 3;
     $mail->Debugoutput = 'html';
 
-    // SMTP Infomaniak
     $mail->isSMTP();
     $mail->Host = 'mail.infomaniak.com';
     $mail->Port = 465;
     $mail->SMTPAuth = true;
     $mail->Username = 'contact@crewup.ch';
     $mail->Password = 'v#10_w!HX-6tLVw';
-
-    // SSL
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->SMTPAutoTLS = false;
 
-    $mail->setFrom('contact@crewup.ch', 'CrewUp SMTP Test');
+    $mail->setFrom('contact@crewup.ch', 'CrewUp test');
     $mail->addAddress('contact@crewup.ch');
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Test SMTP';
-    $mail->Body = '<b>Test OK si tu reçois ça.</b>';
+    $mail->Subject = 'SMTP Test OK';
+    $mail->Body = 'Si tu lis ça, ça marche enfin.';
+    $mail->AltBody = 'SMTP OK';
 
-    echo "Envoi...\n";
     $mail->send();
-    echo "*** MAIL ENVOYÉ ***";
+
+    echo ">>> MAIL ENVOYÉ ✔️";
 
 } catch (Exception $e) {
-    echo "ERREUR : " . $e->getMessage() . "\n";
-    echo "Mailer Error : " . $mail->ErrorInfo . "\n";
+    echo "ERROR : " . $e->getMessage() . "\n";
+    echo "Mailer Error : " . $mail->ErrorInfo;
 }
 
-echo "\nFin du test</pre>";
+echo "\nEND</pre>";
